4fbeddb4ddbe86bc0e7d072f179e89ab535354d9
Max <mopitz199@gmail.com>
2020-07-20 23:30:17 -0400

    .

---
+++ economic_system/settings/prod.py
@@ -0,0 +1,41 @@
+from economic_system.settings.base import *
+
+SECRET_KEY = env('SECRET_KEY')
+
+STATIC_ROOT = "/static_files/"
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+# Sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=env('SENTRY_DNS'),
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
