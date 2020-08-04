95e8a7e95a753fb0c11b3f3bb75b0887eab09f57
Max <mopitz199@gmail.com>
2020-08-04 01:23:59 -0400

    .

--- scripts/api_start_aws.sh
+++ scripts/api_start_aws.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python manage.py migrate postgres_extensions
 python manage.py migrate
 gunicorn -w 4 economic_system.wsgi -b 0.0.0.0:9000 --timeout 1500 --graceful-timeout 1500 --access-logfile /var/log/gunicorn-access.log --error-logfile /var/log/gunicorn-error.log
