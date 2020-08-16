10093c67916cf39b1946d70f8030304b7050983a
Max <mopitz199@gmail.com>
2020-08-15 22:01:24 -0400

    .

--- scripts/celery_beat_start_aws.sh
+++ scripts/celery_beat_start_aws.sh
@@ -2,4 +2,5 @@
 
 python manage.py migrate postgres_extensions
 python manage.py migrate
+sleep 1m
 celery -A economic_system beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- scripts/celery_start_aws.sh
+++ scripts/celery_start_aws.sh
@@ -2,4 +2,5 @@
 
 python manage.py migrate postgres_extensions
 python manage.py migrate
+sleep 1m
 celery -A economic_system worker
