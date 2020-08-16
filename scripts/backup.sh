04553dc077d6ebe357ff35abadf24fb8081f8627
Max <mopitz199@gmail.com>
2020-08-15 21:51:57 -0400

    .

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,12 +7,11 @@ timestamp() {
 postgres_container_id=$(docker ps | grep 'postgres' | awk '{ print $1 }')
 web_container_id=$(docker ps | grep 'web' | awk '{ print $1 }')
 
-base_path=$(pwd)
-cd /mnt/volume_nyc1_01/src/economic_system/backups/
+cd /efs/
 
 file_name=$(timestamp)
 docker exec ${postgres_container_id} pg_dump -U admin economic_system>${file_name}
 
-docker exec ${web_container_id} aws s3 cp backups/${file_name} s3://economicapp-db-backups
+aws s3 cp ${file_name} s3://economicapp-db-backups
 
-rm ${file_name}
+rm -f ${file_name}
